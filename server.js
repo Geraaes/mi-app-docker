ffa201c656bc6582fd8a1f30aedc4a786bb79d18
RashadKanan <rashadkanan08@gmail.com>
2025-10-05 14:31:13 -0600

    Primer commit de Docker

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hola soy Alex");
+  res.send("Hola soy Rashad");
 });
 
 app.listen(PORT, () => {
